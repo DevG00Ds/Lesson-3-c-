568235e453287c61bf25a83dbdc5bfbf5a30aed4
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-11 02:02:37 +0300

    not over yet task4home

--- task4Home/Program.cs
+++ task4Home/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход 3 числа 
+//и выдает максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+string numberA = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string numberB = Console.ReadLine();
+Console.WriteLine("Введите третье число");
+string numberC = Console.ReadLine();
+
+int MAX = 0;
+int a = Convert.ToInt32(numberA);
+int b = Convert.ToInt32(numberB);
+int c = Convert.ToInt32(numberC);
+
+
+if (a > MAX)
+{
+    MAX = a;
+}
+else if (b > MAX)
+{
+    MAX = b;
+}
+else if (c > MAX)
+{
+    MAX = c;
+}
+else
+{
+    Console.WriteLine(" MAX = 0 " + MAX);
+}
