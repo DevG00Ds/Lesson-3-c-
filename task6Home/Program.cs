63c6a15a750438585f99b58b29b88fa3c3f42a20
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-11 12:37:30 +0300

    success work task6Home

--- task6Home/Program.cs
+++ task6Home/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Напишите программу, которая на вход принимает число и выдает,
+// является ли число четным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine( " Введите число: ");
+string answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+int DIV = number % 2;
+
+if ( DIV ==  0 )
+{
+    Console.WriteLine( " Да " );
+}
+else
+{
+    Console.WriteLine( " Нет " );
+}
